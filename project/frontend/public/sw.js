799f17ff7407d0a7fa39b26f2af8504087e9aa68
suvendukungfu <ssuvendukumar489@gmail.com>
2026-03-29 16:05:45 +0530

    Update PWA manifest and service worker

---
+++ project/frontend/public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'axion-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/favicon.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
